003a94782742946948df95d462e4a680e25a67dc
madiazf1 <ma.diazf1@uniandes.edu.co>
2024-09-20 17:43:50 -0500

    Web-005: se define el HTML de la pantalla  de reporte, falta trabajar en el CSS y en la interacción de los botones.

--- web/maqueta-web/src/app/report/report.component.ts
+++ web/maqueta-web/src/app/report/report.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
 
+interface RowData {
+  col1: string;
+  col2: string;
+  col3: string;
+  col4: string;
+  col5: string;
+}
+
 @Component({
   selector: 'app-report',
   templateUrl: './report.component.html',
   styleUrls: ['./report.component.css']
 })
 export class ReportComponent {
+    displayedColumns: string[] = ['col1', 'col2', 'col3', 'col4', 'col5', 'action'];
+    dataSource: RowData[] = [
+      { col1: 'Alarma 1', col2: '25/01/2025', col3: 'Arriendo', col4: '$2 000 000', col5: 'Bancolombia'},
+      { col1: 'Alarma 1', col2: '25/01/2025', col3: 'Arriendo', col4: '$2 000 000', col5: 'Bancolombia'},
+      { col1: 'Alarma 1', col2: '25/01/2025', col3: 'Arriendo', col4: '$2 000 000', col5: 'Bancolombia'},
+      { col1: 'Alarma 1', col2: '25/01/2025', col3: 'Arriendo', col4: '$2 000 000', col5: 'Bancolombia'},
+      { col1: 'Alarma 1', col2: '25/01/2025', col3: 'Arriendo', col4: '$2 000 000', col5: 'Bancolombia'},
+      { col1: 'Alarma 1', col2: '25/01/2025', col3: 'Arriendo', col4: '$2 000 000', col5: 'Bancolombia'},
+      { col1: 'Alarma 1', col2: '25/01/2025', col3: 'Arriendo', col4: '$2 000 000', col5: 'Bancolombia'}
+  ];
 
 }
